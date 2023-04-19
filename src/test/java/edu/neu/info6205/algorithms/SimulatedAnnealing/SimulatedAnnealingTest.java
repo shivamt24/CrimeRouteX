ad372dcae5cc32aad8a6829f3e5ddb118e3b3079
Kaushik Gnanasekar <gnanasekar.ka@northeastern.edu>
2023-04-18 21:53:41 -0400

    updated tests

--- src/test/java/edu/neu/info6205/algorithms/SimulatedAnnealing/SimulatedAnnealingTest.java
+++ src/test/java/edu/neu/info6205/algorithms/SimulatedAnnealing/SimulatedAnnealingTest.java
@@ -1,22 +1,21 @@
+package edu.neu.info6205.algorithms.SimulatedAnnealing;
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 import edu.neu.info6205.SimulatedAnnealing.SimulatedAnnealing;
 
-import static org.junit.Assert.*;
-
-public class SATest {
-
+public class SimulatedAnnealingTest {
     @Test
     public void testSimulateAnnealing() {
-        double startingTemperature = 100;
-        int numberOfIterations = 1000;
-        double coolingRate = 0.99;
+        double startingTemperature = 1000;
+        int numberOfIterations = 10000;
+        double coolingRate = 0.995;
         String filename = "./src/test/resources/inputs/test_sa_read.csv";
         double result = SimulatedAnnealing.simulateAnnealing(startingTemperature, numberOfIterations, coolingRate, filename);
+
         // Check that the result is not null
         assertNotNull(result);
-
-        // Check that the result is greater than 0
-        assertTrue(result == 75796.0);
+        assertTrue(result > 75000);
     }
+
 }
