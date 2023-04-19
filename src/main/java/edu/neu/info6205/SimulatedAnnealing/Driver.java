972a66f8f986057c8164ae4fffb1ae221f4b541e
Kaushik Gnanasekar <gnanasekar.ka@northeastern.edu>
2023-04-18 20:59:46 -0400

    cleanup

--- src/main/java/edu/neu/info6205/SimulatedAnnealing/Driver.java
+++ src/main/java/edu/neu/info6205/SimulatedAnnealing/Driver.java
@@ -3,8 +3,7 @@ package edu.neu.info6205.SimulatedAnnealing;
 public class Driver {
     public static void main(String args[]) {
         System.out.println(
-					"Optimized distance=" + SimulatedAnnealing.simulateAnnealing(1000, 10000, 0.9995));
-
+                "Optimized distance=" + SimulatedAnnealing.simulateAnnealing(1000, 10000, 0.9995));
 
     }
 }
